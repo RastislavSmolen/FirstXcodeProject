61bcb766277c98cc98efe2d792cd26b0690a090f
Rastislav Smolen <>
2021-07-04 19:15:35 +0100

    Initial commit

---
+++ FirstXcodeProject/FirstXcodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstXcodeProject
+//
+//  Created by Rastislav Smolen on 04/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
